c18f0d1a5b6607e89f8db4a3d5dbe4a291b7e860
royor-3 <roy.framery@gmail.com>
2023-11-13 15:58:05 +0100

    implemented a Page to see user profile

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -9,9 +9,16 @@ import { usePathname, useRouter } from "next/navigation"
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete}) => {
   const { data: session } = useSession();
   const pathName = usePathname();
-  // const router = useRouter();
+  const router = useRouter();
 
   const [ copied, setCopied ] = useState("");
+
+  const handleProfileClick = () => {
+
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
 
   const handleCopy = () => {
     setCopied(post.prompt);
@@ -24,14 +31,17 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete}) => {
       className="font-inter text-sm blue_gradient cursor-pointer"
       onClick={() => handleTagClick && handleTagClick(tag) }
     >
-    #{tag}
+      #{tag}
     </p>
     ))
   }
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+        <div 
+          className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+          onClick={handleProfileClick}
+        >
           <Image 
             src={post.creator.image}
             alt="user_image"
